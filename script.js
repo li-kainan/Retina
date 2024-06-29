94d1526c51d3ff38a6fb02e51855bbfdd64f77b8
li-kainan <54035517+li-kainan@users.noreply.github.com>
2024-06-29 15:40:34 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     var ChartConfig
     var LineChart
     
-    const fetch_CSV = (ValueIn) => {
+    async fetch_CSV = (ValueIn) => {
         fileName_path = ['csv/', ValueIn.toString(), '.csv']
         var fileName = fileName_path.join('');
         console.log(fileName)
@@ -21,7 +21,6 @@ document.addEventListener('DOMContentLoaded', () => {
                     dynamicTyping: true,
                     complete: function(results) {
                         CSVData = results
-                        console.log(CSVData)
                     }
                 });
             });
@@ -88,99 +87,19 @@ document.addEventListener('DOMContentLoaded', () => {
         const chart = document.getElementById('chart').getContext('2d');
         LineChart = new Chart(chart, ChartConfig);
     }
-
-    /*
-    const update = () => {
-        const NumberInput = document.getElementById('number-input');
-        ValueIn = parseInt(NumberInput.value)
-        fetch_CSV(ValueIn);
-        get_ChartData();
-        get_ChartConfig();
-        update_Chart();
-    }
-    */
     
     const update = () => {
         const NumberInput = document.getElementById('number-input');
         ValueIn = parseInt(NumberInput.value)
-        
-        fileName_path = ['csv/', ValueIn.toString(), '.csv']
-        var fileName = fileName_path.join('');
-        console.log(fileName)
-        fetch(fileName)
-            .then(response => response.text())
-            .then(data => {
-                Papa.parse(data, {
-                    header: true,
-                    dynamicTyping: true,
-                    complete: function(results) {
-                        CSVData = results
-                        console.log(CSVData)
-                    }
-                });
-            });
-
+        fetch_CSV(ValueIn);
         console.log(CSVData)
-        
-        ChartData = {labels: [], datasets: []};
-        
-        for (let i = 0; i < CSVData.data.length; i++) {
-            ChartData.labels.push(CSVData.data[i]['x'])
-
-            for (let j = 1; j < 11; j++) {
-                var label = 'L' + j.toString()
-                if (i == 0) {
-                    ChartData.datasets.push(
-                        {
-                            label: label,
-                            borderColor: 'rgb(255, 99, 132)',
-                            backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                            pointRadius: 0,
-                            pointHoverRadius: 0,
-                            data: []
-                        }
-                    )
-                }
-                ChartData.datasets[j-1].data.push(CSVData.data[i][label])
-            }
-        }
-
+        get_ChartData();
         console.log(ChartData)
-        
-        ChartConfig = {
-            type: 'line',
-            data: ChartData,
-            options: {
-                responsive: true,
-                scales: {
-                    x: {
-                        display: true,
-                        title: {
-                            display: true,
-                            text: 'x'
-                        }
-                    },
-                    y: {
-                        display: true,
-                        title: {
-                            display: true,
-                            text: 'y'
-                        }
-                    }
-                }
-            }
-        };
-
+        get_ChartConfig();
         console.log(ChartConfig)
-        
-        if (LineChart) {
-            LineChart.destroy();
-            LineChart = null;
-        }
-        const chart = document.getElementById('chart').getContext('2d');
-        LineChart = new Chart(chart, ChartConfig);
-        
+        update_Chart();
     }
+    
         
     ShowButton.addEventListener('click', () => {
         update()
